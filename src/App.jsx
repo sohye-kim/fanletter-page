d7a46133e23d9b05f420ef4848e570bca58d820a
김소혜 <nanasisan@naver.com>
2024-02-04 22:42:17 +0900

    확장자 변경 js->jsx 및 헤더 설정

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header className="header">
+        <img src={logo} className="logo" alt="secondwind bss logo" />
+        <span>Fan letter Page</span>
+      </header>
+      
+    </>
+      
+  );
+}
+
+export default App;
